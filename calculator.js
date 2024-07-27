16e18eba8e2abf6f5613c143730e1d1c1e09d503
Siddharth Mehra <mehrasiddharth1992@gmail.com>
2024-07-27 15:52:43 +0530

    Empty String gives 0 : Test Passed

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
 function addition (numbers) {
     // console.log(numbers)
+    if(!numbers) return 0;
+
     return numbers;
 }
 module.exports = {addition};
--- testcalculator.js
+++ testcalculator.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const {addition} = require('./calculator')
 
-assert.strictEqual(addition(""), "", "Result is Empty String")
+assert.strictEqual(addition(""), 0, "Result is Empty String")
+
 console.log("Test Passed")
