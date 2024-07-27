6dc4aab9a5eb437802a0e9d8a30dddb47aa22b26
Siddharth Mehra <mehrasiddharth1992@gmail.com>
2024-07-27 16:14:53 +0530

    Integers as String : Test Passed

--- calculator.js
+++ calculator.js
@@ -2,6 +2,24 @@ function addition (numbers) {
     // console.log(numbers)
     if(!numbers) return 0;
 
-    return numbers;
+    let num = numbers.split(",")
+
+    // let intnum = parseInt(num);
+    // console.log(intnum);
+
+    // let intnum = num.forEach((val) => {
+    //   let a = parseInt(val);
+    //   return a;
+    // });
+    // console.log(intnum);
+
+    let numint = num.map((val) => {
+        return parseInt(val)
+    })
+    let total = numint.reduce((val, acc) => {
+            return val + acc
+    })
+
+    return total;
 }
 module.exports = {addition};
